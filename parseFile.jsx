fcbef7091a37ed7d6ad55ffaa3d889e284ff6fc7
Cam Nikiel <36055749+crn2357@users.noreply.github.com>
2020-07-17 15:04:16 -0400

    Update parseFile.jsx
    Added comments to parseFile.jsx

--- parseFile.jsx
+++ parseFile.jsx
@@ -4,30 +4,36 @@ import { api } from "./services/api";
 
 class parseFile extends React.Component {
 
+    /* Constructor for SCV file */
     constructor(input){
         super(input);
         this.state = {data: []};
         this.getData = this.getData.bind(this);
     }
 
+    /* parses through CSV file */
     parseable(){
         this.getCsvData();
     }
 
+    /* Gets the CSV file and location to be parsed */
     getCsv(){
         return fetch('/test-files/normal.csv').then(function (response) {
             let reader = response.body.getReader();
             let decoder = new TextDecoder('utf-8');
+            
             return ReadableStreamReader.read().then(function (result) {
                 return decoder.decode(result.value);
             })
         });
     }
 
+    /* Sets the data from the CSV file */
     getData(resultData) {
         this.setStaate({data: resultData.data});
     }
 
+    /* Gets the data from the CSV file */
     async getCsvData(){
         let csvData = await this.getCsv();
         AudioParamMap.parse(csvData, {
@@ -35,6 +41,7 @@ class parseFile extends React.Component {
         });
     }
 
+    /* Displays data from the CSv file */
     render(){
         return (
             <>
